1fc6d23f7e56a1d336be1eaaf22435bce01c2d1f
Bruno Porto <brunotporto@gmail.com>
2016-11-16 18:15:26 -0200

    iniciando codigo para inteligencia de conversao para ransack

--- lib/visual_condition_builder/converter.rb
+++ lib/visual_condition_builder/converter.rb
@@ -4,6 +4,16 @@ module VisualConditionBuilder
       ransack_q = {}
       conditions = params.is_a?(Array) ? params : JSON.parse(params ||= '[]')
       conditions.map do |p|
+        #TODO: Verificar se o código deve ser inserido com inteligência para juntar os campos de mesmo operador
+        # if ransack_q["#{p[0]}_#{p[1]}"].present?
+        #     old_value = ransack_q["#{p[0]}_#{p[1]}"]
+        #     ransack_q.delete("#{p[0]}_#{p[1]}")
+        #     ransack_q["#{p[0]}_in"] = []
+        #     ransack_q["#{p[0]}_in"] << old_value
+        #     ransack_q["#{p[0]}_in"] << p[2]
+        # else
+        #   ransack_q["#{p[0]}_#{p[1]}"] = p[2]
+        # end
         ransack_q["#{p[0]}_#{p[1]}"] = p[2]
       end
       ransack_q
